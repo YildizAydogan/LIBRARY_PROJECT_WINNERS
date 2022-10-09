191ea7f8fa61df44dfd089989cbacf077411671d
YildizAydogan <aydogannyildiz@gmail.com>
2022-10-10 00:00:23 +0200

    10.10.22 by yildiz

---
+++ src/main/java/com/winners/libraryproject/entity/Categories.java
@@ -0,0 +1,50 @@
+package com.winners.libraryproject.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="categories")
+public class Categories {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+
+
+
+    @Size(min= 2, max = 50)
+    @NotNull(message = "Please enter a valid Name!")
+    @Column(nullable = false)
+    private String name;
+
+
+
+    @Column(nullable = false)
+    private Boolean builtIn = false;
+
+
+
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private Integer sequence;
+
+
+
+
+
+}
