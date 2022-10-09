80c6f3964b12cf4225f899395b6fb427a3638a6f
basri <basriakbas@hotmail.com>
2022-10-09 22:42:32 +0200

    first

---
+++ src/test/java/com/winners/libraryproject/LibraryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winners.libraryproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
