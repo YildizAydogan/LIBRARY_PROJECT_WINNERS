b67095735de606c0435c7271fd47aecba51c1724
basri <basriakbas@hotmail.com>
2022-10-12 19:47:48 +0200

    user class endpointler

--- src/main/java/com/winners/libraryproject/repository/BookRepository.java
+++ src/main/java/com/winners/libraryproject/repository/BookRepository.java
@@ -2,6 +2,8 @@ package com.winners.libraryproject.repository;
 
 import com.winners.libraryproject.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book,Long> {
 }
--- src/main/java/com/winners/libraryproject/repository/UserRepository.java
+++ src/main/java/com/winners/libraryproject/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.winners.libraryproject.repository;
 
 import com.winners.libraryproject.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 }
