e925708db4e5020fac8e29951a19196f96619724
Samet Öztürk <0zturksamet4901@gmail.com>
2022-10-13 21:51:03 +0200

    some notes

--- src/main/java/com/winners/libraryproject/entity/enumeration/UserRole.java
+++ src/main/java/com/winners/libraryproject/entity/enumeration/UserRole.java
@@ -2,6 +2,5 @@ package com.winners.libraryproject.entity.enumeration;
 
 public enum UserRole {
 
-    ROLE_ADMIN,ROLE_STAFF,ROLE_MEMBER
-
+    ROLE_ADMIN,ROLE_MEMBER,ROLE_EMPLOYEE,ROLE_ANONYMOUS
 }
