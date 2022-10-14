41d08fe97b31aeffeaa20030af766c04f32d39fe
YildizAydogan <aydogannyildiz@gmail.com>
2022-10-14 20:18:07 +0200

    14.10.22 by yildiz

--- src/main/java/com/winners/libraryproject/repository/UserRepository.java
+++ src/main/java/com/winners/libraryproject/repository/UserRepository.java
@@ -2,9 +2,14 @@ package com.winners.libraryproject.repository;
 import com.winners.libraryproject.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import javax.transaction.Transactional;
+import java.util.Optional;
+@Transactional
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+    //    @Query("SELECT u From User u Where u.email = ?1")
+    Optional<User> findByEmail(String email);
+
 
 
 }
