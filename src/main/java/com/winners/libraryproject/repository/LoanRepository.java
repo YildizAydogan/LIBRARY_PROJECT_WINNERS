c2f4e28892aa523ff534b1e6172137de7d0c8eaf
fsen52 <furkansen.52@gmail.com>
2022-11-11 15:57:57 +0100

    after merge LoanRepository fixed

--- src/main/java/com/winners/libraryproject/repository/LoanRepository.java
+++ src/main/java/com/winners/libraryproject/repository/LoanRepository.java
@@ -1,13 +1,22 @@
 package com.winners.libraryproject.repository;
 
 import com.winners.libraryproject.entity.Loan;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Transactional
 @Repository
 public interface LoanRepository extends JpaRepository<Loan,Long> {
+
+    //-----------------LOANS----search------------------------------
+    @Query("SELECT r from Loan r " +
+            "where r.userId.id=?1")
+    List<Loan> findAllWithPageByUserId(Long userId, Pageable pageable);
+
 
 
 
