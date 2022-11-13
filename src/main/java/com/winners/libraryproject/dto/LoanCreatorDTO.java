1998691cbc3eb6217e6095272b875b4c5fac7dd9
fsen52 <furkansen.52@gmail.com>
2022-11-13 22:11:50 +0100

    Loan endpoinst added

---
+++ src/main/java/com/winners/libraryproject/dto/LoanCreatorDTO.java
@@ -0,0 +1,24 @@
+package com.winners.libraryproject.dto;
+
+import com.winners.libraryproject.entity.Book;
+import com.winners.libraryproject.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Lob;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+public class LoanCreatorDTO {
+
+    private User userId;
+
+    private Book bookId;
+
+    String notes;
+
+
+
+}
--- src/main/java/com/winners/libraryproject/repository/LoanRepository.java
+++ src/main/java/com/winners/libraryproject/repository/LoanRepository.java
@@ -1,25 +1,50 @@
 package com.winners.libraryproject.repository;
 
+import com.winners.libraryproject.dto.LoanDTO;
+import com.winners.libraryproject.entity.Book;
 import com.winners.libraryproject.entity.Loan;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 import javax.transaction.Transactional;
 import java.util.List;
 
 @Transactional
 @Repository
-public interface LoanRepository extends JpaRepository<Loan,Long> {
+public interface LoanRepository extends JpaRepository<Loan, Long> {
 
     //-----------------LOANS----search------------------------------
     @Query("SELECT r from Loan r " +
             "where r.userId.id=?1")
     List<Loan> findAllWithPageByUserId(Long userId, Pageable pageable);
 
+    //-----------------LOANS/:id----------------------------------
+    @Query("SELECT r from Loan r WHERE r.id = ?1 and r.userId.id = ?2")
+    Loan findByIdAndUserId(Long loanId, Long userId);
+
+    //-----------------/loans/user/:userId---------search-------------------------
+    @Query("SELECT r from Loan r " +
+            "where r.userId.id=?1")
+    List<LoanDTO> findLoansByUserIdPage(Long userId, Pageable pageable);
 
 
+    // -----------------/loans/book/:bookId---------search-------------------------
+    @Query("SELECT r from Loan r " +
+            "where r.bookId.id=?1")
+    List<Loan> findAllByBookId(Book book, Pageable pageable);
 
+    //-----------------/loans/auth/:loanId----------------------------------
+        //Jpa repositories methods used
+
+    //-----------------/loans post----------------------------------
+    @Query("SELECT r from Loan r " +
+            "where r.userId.id=?1")
+    List<LoanDTO> findLoansByUserId(Long userId);
+
+    //-----------------/loans/:id put----------------------------------
 
 
 }
